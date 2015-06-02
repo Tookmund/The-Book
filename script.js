916080d1a0adea34d28dc6800b226979aad92fb8
Jacob Adams <Tookmund@users.noreply.github.com>
2015-06-02 10:27:37 -0400

    use set interval instead

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ function bigBrother() {
     var progress = "<div class='progress'><div class='progress-bar progress-bar-danger' role='progressbar' aria-valuenow='10' aria-valuemin='0'aria-valuemax='100' style='width: 10%'>Informing Thought Police</div></div>";
     $("body").html("<div id='bb' align='center'>Big Brother is Watching You</div>"+progress);
     $("body").css("background-color", "black");
-    var value = 10;
-    while (value < 100) {
+    var value = 0;
+    setInterval(function() {
         value += 10;
-        window.setTimeout(function() {
-            $('.progress-bar').css('width',value+'%');
-            console.log(value);
+        if (value > 100) {
+            value = 0;
+        }
+        $('.progress-bar').css('width',value+'%');
+        console.log(value);
         },1000);
         
     }
