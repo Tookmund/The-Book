a1049da861fa59686a7b858a92f832c2b53d9171
Jacob Adams <Tookmund@users.noreply.github.com>
2015-06-02 10:31:52 -0400

    Striped progress bar and better progress

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function bigBrother() {
-    var progress = "<div class='progress'><div class='progress-bar progress-bar-danger' role='progressbar' aria-valuenow='10' aria-valuemin='0'aria-valuemax='100' style='width: 10%'>Informing Thought Police</div></div>";
+    var progress = "<div class='progress'><div class='progress-bar progress-bar-danger progress-bar-striped' role='progressbar' aria-valuenow='10' aria-valuemin='0'aria-valuemax='100' style='width: 10%'>Informing Thought Police</div></div>";
     $("body").html("<div id='bb' align='center'>Big Brother is Watching You</div>"+progress);
     $("body").css("background-color", "black");
     var value = 0;
     setInterval(function() {
-        value += 10;
+        value += 100;
         if (value > 100) {
             value = 0;
         }
