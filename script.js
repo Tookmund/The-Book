13480876d624e7b24337bccaf7be31e01e277983
Jacob Adams <Tookmund@users.noreply.github.com>
2015-06-02 10:37:10 -0400

    start at 0 instead of 10

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function bigBrother() {
-    var progress = "<div class='progress'><div class='progress-bar progress-bar-danger progress-bar-striped' role='progressbar' aria-valuenow='10' aria-valuemin='0'aria-valuemax='100' style='width: 10%'>Informing Thought Police</div></div>";
+    var progress = "<div class='progress'><div class='progress-bar progress-bar-danger progress-bar-striped' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width: 0%'>Informing Thought Police</div></div>";
     $("body").html("<div id='bb' align='center'>Big Brother is Watching You</div>"+progress);
     $("body").css("background-color", "black");
     var value = 0;
