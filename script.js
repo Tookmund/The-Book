4bf7eecbc7089b7bae80918db853fc29bef9ef4f
Jacob Adams <Tookmund@users.noreply.github.com>
2015-06-02 10:16:33 -0400

    Try making a progress bar

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function bigBrother() {
-    $("body").html("<div id='bb' align='center'>Big Brother is Watching You</div>");
+    var progress = "<div class='progress'><div class='progress-bar progress-bar-danger' role='progressbar' aria-valuenow='10' aria-valuemin='0'aria-valuemax='100' style='width: 10%'>Informing Thought Police</div></div>";
+    $("body").html("<div id='bb' align='center'>Big Brother is Watching You</div>"+progress);
     $("body").css("background-color", "black");
+    var value = 10;
+    $('.progress').change(function(event) {
+        value = $(this).val();
+        $('.progress-bar').css('width',value+10);
+    });
+    $('.progress-bar').css('width',value+10);
 }
