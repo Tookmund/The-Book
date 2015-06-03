a818431657b2f5e2840b261d9ae9db64555ceb47
Jacob Adams <Tookmund@users.noreply.github.com>
2015-06-03 18:52:05 -0400

    init carousel

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+$(document).ready(function() {
+    $('.myCarousel').carousel();
+});
 function bigBrother() {
     var progress = "<div class='progress'><div class='progress-bar progress-bar-danger progress-bar-striped' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width: 0%'>Informing Thought Police</div></div>";
     $("body").html("<div id='bb' align='center'>Big Brother is Watching You</div>"+progress);
