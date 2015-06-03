b59bf69b35e9a8929b3ea93f2c3ad5b8a9eefef1
Jacob Adams <Tookmund@users.noreply.github.com>
2015-06-03 18:53:05 -0400

    Silly Typo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.myCarousel').carousel();
+    $('#myCarousel').carousel();
 });
 function bigBrother() {
     var progress = "<div class='progress'><div class='progress-bar progress-bar-danger progress-bar-striped' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width: 0%'>Informing Thought Police</div></div>";
