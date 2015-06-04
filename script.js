8baaa071d99de771b01b8a6e47369ff610f248ae
Jacob Adams <Tookmund@users.noreply.github.com>
2015-06-03 21:27:54 -0400

    clean up my mess

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-$(document).ready(function() {
-    $('#myCarousel').carousel();
-});
 function bigBrother() {
     var progress = "<div class='progress'><div class='progress-bar progress-bar-danger progress-bar-striped' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width: 0%'>Informing Thought Police</div></div>";
     $("body").html("<div id='bb' align='center'>Big Brother is Watching You</div>"+progress);
     $("body").css("background-color", "black");
+    
     var value = 0;
     setInterval(function() {
         value += 1;
@@ -12,7 +10,6 @@ function bigBrother() {
             value = 100;
         }
         $('.progress-bar').css('width',value+'%');
-        console.log(value);
         },100);
         
     }
